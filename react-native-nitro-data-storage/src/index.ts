10a34304688d22a45374d419133c742e305a34f7
Marc Rousavy <me@mrousavy.com>
2025-12-04 12:10:42 +0100

    finish the nitro implementation

--- react-native-nitro-data-storage/src/index.ts
+++ react-native-nitro-data-storage/src/index.ts
@@ -1,4 +1,4 @@
 import { NitroModules } from 'react-native-nitro-modules';
 import { DataStorage } from './specs/NitroDataStorage.nitro';
 
-export const NitroDataStorage = NitroModules.createHybridObject<DataStorage>('NitroDataStorage')
+export const NitroDataStorage = NitroModules.createHybridObject<DataStorage>('DataStorage')
